4b7870d1aecf4e5ad7663b94158ca39363449710
Franklin-pro <136065526+Franklin-pro@users.noreply.github.com>
2024-04-07 00:56:43 +0200

    ordering completed

--- src/components/scams/didItems/didTable.jsx
+++ src/components/scams/didItems/didTable.jsx
@@ -7,9 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
 import DeleteIcon from '@mui/icons-material/Delete';
-
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
@@ -37,13 +35,13 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 
 
-export default function OrderTables() {
-  const [product, setProduct]= useState([])
+export default function DidTables() {
+  const [users,setUsers]= useState(null)
 
   const handleDeleted = async (itemsId) => {
     try {
         const token = localStorage.getItem('token');
-        const response = await fetch(`http://localhost:3030/API/order/${itemsId}`, {
+        const response = await fetch(`https://masterkraft-bn.onrender.com/API/didyou/delete/${itemsId}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
@@ -72,59 +70,56 @@ export default function OrderTables() {
 }
 
   useEffect(()=>{
-    const fetchProduct = async ()=>{
-       const response = await axios.get(`http://localhost:3030/API/order`)
-       setProduct(response.data)
+    const fetchDatas = async () =>{
+     try {
+      const token = localStorage.getItem('token')
+       const response = await axios.get(`https://masterkraft-bn.onrender.com/API/didyou/get`,{
+     headers: {
+      'auth-token':token,
+      'Content-Type': 'application/json',
+     }
+       })
+       setUsers(response.data);
+       // console.log(response.data)
+     } catch (error) {
+      alert(error.response.data.message)
+     }
     };
-    fetchProduct([])
+    fetchDatas([])
   },[])
-  
-  console.log(product)
+  console.log(users)
   return (
     <>
     <div className='items'>
-    <h1>ORDERING ITEMS</h1>
+    <h1>USER SIGNED IN</h1>
     </div>
    
 <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell style={{fontSize:'1.8rem'}}>NO</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>QUANTITY</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>PRODUCT-NAME</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>PRICE</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>NUMBER</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>SHIPPINGADDRESS</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>TOTAL PRICE</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>EMAIL</StyledTableCell>
-            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>TIME</StyledTableCell>
+          <StyledTableCell style={{fontSize:'1.8rem'}}>NO</StyledTableCell>
+            <StyledTableCell style={{fontSize:'1.8rem'}}>DID YOU KNOW TITLE</StyledTableCell>
+            <StyledTableCell align="right" style={{fontSize:'1.4rem'}}>DID YOU KNOW DESCRIPTION</StyledTableCell>
+            
+           
             <StyledTableCell align="center" style={{fontSize:'1.4rem',backgroundColor:'#fdc800'}} colSpan={2}>OPTIONS</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {product && product.datas && product.datas.map((row,index) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {index +1}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.quantity}</StyledTableCell>
-              <StyledTableCell align="right">{row.productname}</StyledTableCell>
-              <StyledTableCell align="right">{row.productprice}</StyledTableCell>
-              <StyledTableCell align="right">{row.phoneNumber}</StyledTableCell>
-              <StyledTableCell align="right">{row.shippingAddress}</StyledTableCell>
-              <StyledTableCell align="right">{row.totalPrice}</StyledTableCell>
-              <StyledTableCell align="right">{row.email}</StyledTableCell>
-              <StyledTableCell align="right">{row.oderAt}</StyledTableCell>
-              <div className='king'>
-              <DeleteIcon className='iconx delete' onClick={() => handleDeleted(row._id)} />
-            
-              </div>
-              
-             
-            </StyledTableRow>
-          ))}
-        </TableBody>
+  {users && users.datas && users.datas.map((row, index) => (
+    <StyledTableRow key={row.name}>
+      <StyledTableCell component="th" scope="row">
+        {index + 1} 
+      </StyledTableCell>
+      <StyledTableCell align="right">{row.Title}</StyledTableCell>
+      <StyledTableCell align="right">{row.Description}</StyledTableCell>
+      <div className='king'>
+        <DeleteIcon className='iconx delete' onClick={() => handleDeleted(row._id)} />
+      </div>
+    </StyledTableRow>
+  ))}
+</TableBody>
       </Table>
     </TableContainer>
    
